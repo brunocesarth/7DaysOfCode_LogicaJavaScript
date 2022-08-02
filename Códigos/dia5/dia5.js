833a5e461781da0adccc927dfaf9fa73f79c9e0a
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-01 21:38:23 -0300

    Dia 5 - Desafio JS

---
+++ Códigos/dia5/dia5.js
@@ -0,0 +1,12 @@
+let frutas = [];
+let laticinios = [];
+let congelados = [];
+let doces = [];
+let addItem;
+let item;
+
+addItem = prompt("Deseja adicionar uma comida na sua lista de compras? Responda: Sim ou Não");
+
+while (addItem == 'Sim') {
+
+}
