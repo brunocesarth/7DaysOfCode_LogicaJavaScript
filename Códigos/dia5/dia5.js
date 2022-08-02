08821d17a0d763750d8c4aa6b18e6da570732e58
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-02 09:09:20 -0300

    Finalização dia 5 - 7 Days of code

--- Códigos/dia5/dia5.js
+++ Códigos/dia5/dia5.js
@@ -2,11 +2,30 @@ let frutas = [];
 let laticinios = [];
 let congelados = [];
 let doces = [];
-let addItem;
+let addItem = "Sim";
 let item;
+let categoria;
 
-addItem = prompt("Deseja adicionar uma comida na sua lista de compras? Responda: Sim ou Não");
-
-while (addItem == 'Sim') {
-
+while (addItem == "Sim" || addItem == "sim") {
+    addItem = prompt("Deseja adicionar uma comida na sua lista de compras? Responda: Sim ou Não");
+    if (addItem == "Sim" || addItem == "sim") {
+        comida = prompt("Qual comida você deseja inserir?");
+        categoria = prompt("Em qual categoria essa comida se encaixa? frutas, laticínios, congelados, doces");
+        if (categoria == "Frutas" || categoria == "frutas") {
+            frutas.push(comida)
+        } else if (categoria == "Laticinios" || categoria == "laticinios") {
+            laticinios.push(comida);
+        } else if (categoria == "Congelados" || categoria == "congelados") {
+            congelados.push(comida);
+        } else if (categoria == "Doces" || categoria == "doces") {
+            doces.push(comida);
+        }
+    } else if (addItem == "Não" || addItem == "não") {
+        alert(
+            `Frutas: ${frutas}
+            Laticinios: ${laticinios}
+            Congelados: ${congelados}
+            Doces: ${doces}`
+        );
+    }
 }
